f6feac171341e7a054f7c9cc175f50ebb8743c6c
aayanksinghai <aayanksinghai02@gmail.com>
2022-09-05 12:49:11 +0530

    Init Setup Project

---
+++ src/test/java/io/project/smartcontactmanager/SmartcontactmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.project.smartcontactmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartcontactmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
