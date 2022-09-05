52c3fa423341aeb871213ff60690953fa00bea25
aayanksinghai <aayanksinghai02@gmail.com>
2022-09-05 15:40:53 +0530

    Thymeleaf Startup

--- src/main/java/io/project/smartcontactmanager/controller/HomeController.java
+++ src/main/java/io/project/smartcontactmanager/controller/HomeController.java
@@ -3,16 +3,22 @@ package io.project.smartcontactmanager.controller;
 import io.project.smartcontactmanager.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
-//    @Autowired
-//    private UserRepository userRepository;
+    @RequestMapping("/home")
+    public String home(Model model){
+        model.addAttribute("title", "Home - Smart Contact Manager");
+        return "home";
+    }
 
-    @GetMapping("/home")
-    public String hello(){
-        return "Testing Hello";
+    @RequestMapping("/about")
+    public String about(Model model){
+        model.addAttribute("title", "About");
+        return "about";
     }
 }
