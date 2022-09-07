7b77e842f4d1e198adee0905c4f79961c90f3019
aayanksinghai <aayanksinghai02@gmail.com>
2022-09-07 18:13:17 +0530

    Added Contact Functionality

--- src/main/java/io/project/smartcontactmanager/controller/UserController.java
+++ src/main/java/io/project/smartcontactmanager/controller/UserController.java
@@ -1,11 +1,15 @@
 package io.project.smartcontactmanager.controller;
 
 
+import io.project.smartcontactmanager.model.Contact;
 import io.project.smartcontactmanager.model.User;
 import io.project.smartcontactmanager.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.security.Principal;
@@ -16,14 +20,45 @@ public class UserController {
 
     @Autowired
     private UserRepository userRepository;
-    @RequestMapping("/index")
-    public String dashboard(Model model, Principal principal){
+
+//    Har bar chal jayega
+    @ModelAttribute
+    public void addCommonData(Model model, Principal principal){
         String userName = principal.getName();
         System.out.println(userName);
         User user = userRepository.getUserByUserName(userName);
 
         System.out.println(user);
         model.addAttribute("user", user);
+    }
+    @RequestMapping("/index")
+    public String dashboard(Model model, Principal principal){
+
+        model.addAttribute("title", "User Dashboard");
         return "normal/user_dashboard";
+    }
+
+//    Open form handler
+    @GetMapping("/add-contact")
+    public String openAddContactForm(Model model){
+        model.addAttribute("title", "Add Contact");
+        model.addAttribute("contact", new Contact());
+        return "normal/add_contact_form";
+    }
+
+//    processing Add Contact form
+    @PostMapping("/process-contact")
+    public String processContact(@ModelAttribute Contact contact, Principal principal){
+        String name = principal.getName();
+        User user = this.userRepository.getUserByUserName(name);
+
+        contact.setUser(user);
+        user.getContacts().add(contact);
+
+        this.userRepository.save(user);
+
+        System.out.println("Date " + contact);
+        System.out.println("Added to Database");
+        return "normal/add_contact_form";
     }
 }
